b52533e0ca67e952a021b4e8bc4780a757855920
VipinXRana <vipinranavip97@gmail.com>
2025-11-23 22:29:16 +0530

    transporter fix

--- routes/auth.js
+++ routes/auth.js
@@ -47,14 +47,27 @@ const authMiddleware = (req, res, next) => {
 };
 
 // ========== Nodemailer Setup ==========
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
+
 
 // ========== Request OTP ==========
 router.post("/request-otp", async (req, res) => {
